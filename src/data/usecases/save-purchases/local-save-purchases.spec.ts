136edd8c7b3d18b7ff117a31c50e9412e71a1610
Nathan Seixeiro <108758693+nathanSeixeiro@users.noreply.github.com>
2023-07-07 14:23:43 +0000

    use await on async test with promisse and throw error

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -28,12 +28,12 @@ describe('LocalSavedPurchases', () => {
         expect(cacheStore.deleteKey).toBe('purchases')
     })
     
-    test('Should not insert new Cache if delete fails', () => {
+    test('Should not insert new Cache if delete fails', async () => {
         const { cacheStore, sut } = makeSut()
         cacheStore.simuleteDeleteError()
         const promise = sut.save(mockPurchases())
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete])
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     })
     
     test('Should insert new  cache if delete succeds', async () => {
@@ -45,12 +45,12 @@ describe('LocalSavedPurchases', () => {
         expect(cacheStore.insertValues).toEqual(purchases)
     })
     
-    test('Should throws if insert throws', () => {
+    test('Should throws if insert throws', async () => {
         const { cacheStore, sut } = makeSut()
         cacheStore.simuleteInsertError()
         const promise = sut.save(mockPurchases())
         expect(cacheStore.messages).toEqual([CacheStoreSpy.Message.delete, CacheStoreSpy.Message.insert])
-        expect(promise).rejects.toThrow()
+        await expect(promise).rejects.toThrow()
     })
 
 })
